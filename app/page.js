677f7849507e8c118fbb74d90bfc473c015f3fb8
markuskrash <markgrechnev@gmail.com>
2024-03-25 02:47:20 +0300

    6 дз(3)

--- app/page.js
+++ app/page.js
@@ -3,19 +3,28 @@ import "./globals.css"
 import Banner from "./components/Banner/Banner";
 import Promo from "./components/Promo/Promo";
 import {endpoints} from "@/app/api/config";
-import {getNormalizedGamesDataByCategory} from "@/app/api/api-utils";
 import {CardsListSection} from "@/app/components/CardsListSection/CardsListSection";
+import {Preloader} from "@/app/components/Preloader/Preloader";
+import {useGetDataByCategory} from "@/app/api/api-hooks";
 
 
-export default async function Home() {
-    const popularGames = await getNormalizedGamesDataByCategory(endpoints.games, "popular");
-    const newGames = await getNormalizedGamesDataByCategory(endpoints.games, "new");
+export default function Home() {
+    const popularGames = useGetDataByCategory(endpoints.games, "popular");
+    const newGames = useGetDataByCategory(endpoints.games, "new");
 
     return (
         <main className="main">
             <Banner/>
-            <CardsListSection type="slider" id="popular" title="Популярные" data={popularGames}/>
-            <CardsListSection type="slider" id="new" title="Новинки" data={newGames}/>
+            {popularGames?(
+                <CardsListSection type="slider" id="popular" title="Популярные" data={popularGames}/>
+                ) : (
+                    <Preloader />
+                )}
+            {newGames?(
+                <CardsListSection type="slider" id="new" title="Новинки" data={newGames}/>
+            ) : (
+                <Preloader />
+            )}
             <Promo/>
         </main>
     );
